44d95d43773f3a4205a2352c05c32cf298af606a
Cheri Hansen <coolcheri87@gmail.com>
2022-01-22 11:42:52 -0800

    adding comments

--- W03-HiLo/game/card.py
+++ W03-HiLo/game/card.py
@@ -3,12 +3,13 @@
 # Created 1/21/2022
 
 import random
-
+# This is a card class
 class Card:
 
     def __init__(self):
         self.cardValue = random.randint(1,13)
-
+        
+# draw card from 1 to 13
     def drawCard(self):
         self.cardValue = random.randint(1,13)
         return self.cardValue
--- W03-HiLo/game/interface.py
+++ W03-HiLo/game/interface.py
@@ -4,22 +4,27 @@
 
 from .card import Card
 
+
+# This is Interface to play hilo game
 class Interface:
 
+    # This is initializing score and card
     def __init__(self):
         self.score = int(300)
         self.card = Card()
 
-
+    # This is where the game is played
     def start_game(self):
         done = False
+
+        # This is main game loop
         while (not done):
             # Get the previous drawn card and output
             prevValue = int(self.card.cardValue)
             print("The card is: " + str(prevValue))
 
             # Guess whether the card is lower or higher
-            hl = input("Higher or lower? [h/l] ")
+            hl = self.getHigherLower()
 
             # Get next card value and see if we get or lose points
             newValue = int(self.card.drawCard())
@@ -49,9 +54,18 @@ class Interface:
 
     def playAgain(self):
         validInput = False
-
+        #loop until player is done with game
         while (not validInput):
             again = input("Play again? [y/n] ").lower()
             if ((again == 'y') or (again == 'n')):
                 validInput = False
                 return again
+
+    def getHigherLower(self):
+        validInput = False
+        #loop until player has right input
+        while (not validInput):
+            hilo = input("higher or lower [h/l] ").lower()
+            if ((hilo == 'h') or (hilo == 'l')):
+                validInput = False
+                return hilo
