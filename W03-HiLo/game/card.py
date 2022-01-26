4bbfecbcf9be0dc34113bf6c5c149bc88d50bcc9
Cheri Hansen <coolcheri87@gmail.com>
2022-01-25 22:31:29 -0800

    Add lots of comments

--- W03-HiLo/game/card.py
+++ W03-HiLo/game/card.py
@@ -3,9 +3,17 @@
 # Created 1/21/2022 
 
 import random
-    # This is a card class
+
+
+#ToDo: Create Cards class as follows:
+""" 
+Generates Initial card
+compare card enter by user with initial card (high or low)
+return 100 pts or -75 pts
+"""
+# This is a card class
 class Card:
-    
+
     #This is the function to choose random number from 1 thur 13
     def __init__(self):
         self.cardValue = random.randint(1,13)
