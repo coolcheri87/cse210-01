9a32c3295426788ffa65c21a4526008feaeef17e
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2022-02-19 21:38:42 -0800

    Better inheritance

--- W07-Greed/faller/faller.py
+++ W07-Greed/faller/faller.py
@@ -1,9 +1,11 @@
 
+from .gravity import Gravity
 
 class Faller:
 
     def __init__(self):
         self._height = 0
+        self._gravity = Gravity()
 
     def getHeight(self):
         return self._height
@@ -12,5 +14,8 @@ class Faller:
         self._height = 0
         return 0
 
-    def getImpact(self):
-        return 0
+    def fall(self):
+        self._height += self._gravity.getFall()
+        if (self._height > self._maxHeight):
+            self._height = self._maxHeight
+        return self._height
--- W07-Greed/faller/rock.py
+++ W07-Greed/faller/rock.py
@@ -8,17 +8,10 @@ class Rock(Faller):
 
     def __init__(self,maxHeight):
         super().__init__()
-        self.gravity = Gravity()
-        self.gravity.setJupiterGravity
+        self._gravity.setJupiterGravity
         self._maxHeight = maxHeight
         # Setup random position in the top two thirds of play
         self._height = random.randint(0,maxHeight*2/3)
-
-    def fall(self):
-        self._height += self.gravity.getFall()
-        if (self._height > self._maxHeight):
-            self._height = self._maxHeight
-        return self._height
 
     def getImpact(self):
         return -1
